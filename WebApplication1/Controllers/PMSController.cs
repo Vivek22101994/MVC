e8432d48359bbe138377d4acc747deb84afeb008
Vivek221094 <parikhvivek473@gmail.com>
2025-04-26 19:12:05 +0530

    Changes

---
+++ WebApplication1/Controllers/PMSController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PMSController : Controller
+    {
+        // GET: PMS
+        public ActionResult Index()
+        {
+            ViewBag.test = "Abc";
+            ViewBag.Adding = "fff";
+            return View();
+        }
+
+
+    }
+}
