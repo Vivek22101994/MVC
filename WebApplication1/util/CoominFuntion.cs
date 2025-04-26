199b4514bda7edcd0c3eee4303ddff4a5901a4a5
Vivek221094 <parikhvivek473@gmail.com>
2025-04-26 19:22:56 +0530

    Coomoun Function Change

---
+++ WebApplication1/util/CoominFuntion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+public static class CoominFuntion
+{
+    public static string GetTime()
+    {
+        return System.DateTime.Now.ToString("dd/MM/YYYY");
+    }
+}
